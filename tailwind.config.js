96b7e647c65041727d9ed7f3c482b949d32493a5
Nathan Bombana <nathan.pbombana@gmail.com>
2021-05-23 15:42:02 -0300

    The absolute simple landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 module.exports = {
   purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      invert: ['dark'],
+      opacity: ['dark']
+    },
   },
   plugins: [],
 }
